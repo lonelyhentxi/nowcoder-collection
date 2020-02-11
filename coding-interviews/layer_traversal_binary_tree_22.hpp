118a1d970a1898274a09b66eb9f00bb375f2a905
lonelyhentai <master@evernightfireworks.com>
2020-02-11 23:43:00 +0800

    feat(coding-interviews): task 22 layer traversal binary tree & task 23 verify sequence of bst

---
+++ coding-interviews/layer_traversal_binary_tree_22.hpp
@@ -0,0 +1,27 @@
+// [从上往下打印二叉树](https://www.nowcoder.com/practice/7fe2212963db4790b57431d9ed259701)
+#include <vector>
+#include <deque>
+#include "util_tree_node.hpp"
+#define let const auto
+
+namespace task22 {
+    using namespace std;
+    class Solution {
+    public:
+        vector<int> PrintFromTopToBottom(TreeNode* root) {
+            auto res = vector<int>{};
+            auto q = deque<TreeNode*>{};
+            q.push_back(root);
+            while(!q.empty()) {
+                auto current = q.front();
+                q.pop_front();
+                if(current!=nullptr) {
+                    res.push_back(current->val);
+                    q.push_back(current->left);
+                    q.push_back(current->right);
+                }
+            }
+            return res;
+        }
+    };
+}
---
+++ coding-interviews/verify_squence_of_bst_23.hpp
@@ -0,0 +1,37 @@
+// []()
+#pragma once
+#define let const auto
+#include <vector>
+#include "util_tree_node.hpp"
+
+namespace task23 {
+    using namespace std;
+    class Solution {
+    public:
+        bool VerifySquenceOfBST(const vector<int>& sequence) {
+            if(sequence.size()==0) {
+                return false;
+            }
+            return verifySequenceRec(sequence, 0, sequence.size());
+        }
+
+        bool verifySequenceRec(const vector<int>& sequence, size_t start, size_t end) {
+            let len = end - start;
+            if(len<=2) {
+                return true;
+            }
+            let mid_val = sequence[end-1];
+            auto mid_pos = end - 1;
+            for(auto i=0;i < end - 1; i++) {
+                let val = sequence[i];
+                if(val<mid_val && mid_pos != end - 1) {
+                    return false;
+                }
+                else if(val>mid_val && mid_pos == end - 1) {
+                    mid_pos = i;
+                }
+            }
+            return verifySequenceRec(sequence, start, mid_pos) && verifySequenceRec(sequence,mid_pos, end-1);
+        }
+    };
+}
