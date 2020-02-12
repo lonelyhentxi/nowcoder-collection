be8eace963aa69ca1aea67f6ae191bed242ce3ea
lonelyhentai <master@evernightfireworks.com>
2020-02-12 16:28:26 +0800

    feat(coding-interviews): task 24 find path with expect sum & task 25 clone list with random pointer

---
+++ coding-interviews/clone_lists_with_random_pointer_25.hpp
@@ -0,0 +1,52 @@
+// [复杂链表的复制](https://www.nowcoder.com/practice/f836b2c43afc4b35ad6adc41ec941dba)
+
+#define let const auto
+
+namespace task25 {
+    using namespace std;struct RandomListNode {
+        int label;
+        struct RandomListNode *next, *random;
+        RandomListNode(int x) :
+                label(x), next(nullptr), random(nullptr) {
+        }
+    };
+
+    class Solution {
+    public:
+        RandomListNode* Clone(RandomListNode* pHead)
+        {
+            if(pHead == nullptr) {
+                return nullptr;
+            }
+            RandomListNode *current = pHead;
+            while(current!=nullptr) {
+                RandomListNode* new_node = new RandomListNode(current->label);
+                new_node -> next = current -> next;
+                current -> next = new_node;
+                current = current -> next -> next;
+            }
+            current = pHead;
+            while(current!=nullptr) {
+                RandomListNode *new_random = nullptr;
+                if(current->random!=nullptr) {
+                    new_random = current->random->next;
+                }
+                current -> next -> random = new_random;
+                current = current->next->next;
+            }
+            current = pHead;
+            RandomListNode *res = current -> next;
+            while(current!=nullptr) {
+                RandomListNode* origin_next = current->next->next;
+                RandomListNode* new_next = nullptr;
+                if(origin_next!=nullptr) {
+                    new_next = origin_next -> next;
+                }
+                current -> next -> next = new_next;
+                current -> next = origin_next;
+                current = current -> next;
+            }
+            return res;
+        }
+    };
+}
---
+++ coding-interviews/find_path_with_expect_sum_24.hpp
@@ -0,0 +1,41 @@
+// [二叉树中和为某一值的路径](https://www.nowcoder.com/practice/b736e784e3e34731af99065031301bca)
+
+#include <algorithm>
+#define let const auto
+#include <util_tree_node.hpp>
+#include <vector>
+
+namespace task24 {
+    using namespace std;
+    class Solution {
+    public:
+        vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+            if(root==nullptr) {
+                return {};
+            }
+            auto res = vector<vector<int>>{};
+            find_path_rec(root, res, {}, expectNumber);
+            std::sort(res.begin(), res.end(),  [](const vector<int>& a, const vector<int>& b) { return a.size() > b.size(); });
+            return res;
+        }
+
+        // check root nullable out
+        void find_path_rec(TreeNode* root, vector<vector<int>>& paths, vector<int> current_path, int target) {
+            let remain_target= target - (root->val);
+            current_path.push_back(root->val);
+            if(root->left==nullptr && root->right==nullptr) {
+                if(remain_target==0) {
+                    paths.push_back(std::move(current_path));
+                }
+            }
+            else {
+                if(root->left!=nullptr) {
+                    find_path_rec(root->left, paths, current_path, remain_target);
+                }
+                if(root->right!=nullptr) {
+                    find_path_rec(root->right, paths, std::move(current_path),remain_target);
+                }
+            }
+        }
+    };
+}
