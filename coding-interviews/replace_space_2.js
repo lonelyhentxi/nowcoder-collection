8862f9364f1138a47296cf3fc9f3a7b14b30b5c8
lonelyhentai <master@evernightfireworks.com>
2020-02-27 12:30:00 +0800

    feat(code-interviews): task 1 search in sorted 2d arrays & task 2 replace spaces

---
+++ coding-interviews/replace_space_2.js
@@ -0,0 +1,9 @@
+// [替换空格](https://www.nowcoder.com/practice/4060ac7e3e404ad1a894ef3e17650423)
+
+function replaceSpace(str)
+{
+    return str.replace(/ /g, '%20');
+}
+module.exports = {
+    replaceSpace : replaceSpace
+};
